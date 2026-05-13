095a5cb0d8924d48a1abc7618ddbbeb722022024
Ailton-exe <ailton.ramos23b@udgvirtual.udg.mx>
2026-05-13 14:10:11 -0600

    3.7. Integración de base de datos 

--- lib/main.dart
+++ lib/main.dart
@@ -1,92 +1,42 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: PantallaPrincipal(),
+      debugShowCheckedModeBanner: false,
+      home: PantallaFirebase(),
     );
   }
 }
 
-class PantallaPrincipal extends StatelessWidget {
-  const PantallaPrincipal({super.key});
-
+class PantallaFirebase extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(         
+      appBar: AppBar(
         title: const Center(
-          child: Text('Vinil Sentimental'),
-        ),
-        actions: <Widget>[
-          IconButton(
-            icon: const Icon(Icons.menu),
-            tooltip: 'Menu',
-            onPressed: () {
-              ScaffoldMessenger.of(context).showSnackBar(
-                const SnackBar(content: Text('Aquí se mostraría el menú de navegación')),
-              );
-            },
-          ),
-        ],
+          child: Text("Firebase en Flutter")),
       ),
-
-
-      body: Stack(
-         children: [
-                    // Fondo
-                    Container(
-                      decoration: const BoxDecoration(
-                        image: DecorationImage(
-                          image: NetworkImage(
-                            'https://planisferio.com.mx/wp-content/uploads/2025/08/mujer-cubrio-cara-disco-vinilo-retro-entonado_77190-7218.jpg',
-                          ),
-                          fit: BoxFit.cover,
-                        ),
-                      ),
-                    ),
-
-          Center(
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                const Icon(Icons.music_note, size: 50, color: Colors.white),
-                const SizedBox(height: 16),
-                const Text(
-                  '¡Bienvenido a Vinil Sentimental!',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    fontSize: 24,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.white,                    
-                  ),
-                ),
-                SizedBox(height: 20),
-                TextButton(
-                  style: TextButton.styleFrom(
-                    backgroundColor: Colors.white,
-                    padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-                    
-                  ),
-                  onPressed: () {
-                    print('Agregar vinil');
-                  },
-                  child: Text(
-                    'Agregar nuevo vinil',
-                    style: TextStyle(fontSize: 18, color: Colors.blue),
-                  ),
-                ),
-              ],
-            ),
-          ),
-        ],                
+      body: Center(
+        child: Text(
+          "Firebase conectado correctamente",
+          style: TextStyle(fontSize: 24),
+          textAlign: TextAlign.center,
+        ),
       ),
     );
   }
